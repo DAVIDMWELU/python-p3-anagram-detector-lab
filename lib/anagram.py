bceac5c6f5d692c38181d4086856e23eac7a69d0
DAVIDMWELU <davidmwelu@gmail.com>
2025-04-05 10:54:20 +0300

    anagram

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word  # Single word passed at initialization
+
+    def match(self, word_list):
+        '''Returns a list of words that are anagrams of the initialized word.'''
+        anagrams = []
+        for word in word_list:
+            if sorted(self.word) == sorted(word):  # Compare sorted letters
+                anagrams.append(word)
+        return anagrams
